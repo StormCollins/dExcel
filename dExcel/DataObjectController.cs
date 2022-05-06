4b5b1da51313936a7705e01d03681d74b0b55649
Storm <stcollins@deloitte.co.za>
2022-05-06 15:08:33 +0200

    Added functionality to handle DataObjects in Excel.

---
+++ dExcel/DataObjectController.cs
@@ -0,0 +1,41 @@
+﻿namespace dExcel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+public static class DataObjectController
+{
+    private static Dictionary<string, object> dataObjects = new();
+
+    public static string Add(string handle, object dataObject)
+    {
+        char[] bannedCharacters = new char[] { '@', ':' };
+        if (bannedCharacters.Any(handle.Contains))
+        {
+            return $"Handle may not contain: {string.Join(", ", bannedCharacters)}";
+        }
+        if (!dataObjects.ContainsKey(handle))
+        {
+            dataObjects.Add(handle, dataObject);
+        }
+        else
+        {
+            dataObjects[handle] = dataObject;  
+        }
+        return $"@@{handle}:{DateTime.Now:HH:mm:ss}";
+    }
+
+    public static string CleanHandle(string dirtyTag)
+    {
+        return Regex.Match(dirtyTag, @"(?<=@@)[^:]+").Value;
+    }
+
+    public static object GetDataObject(string handle)
+    {
+        return dataObjects[CleanHandle(handle)];
+    }
+}
