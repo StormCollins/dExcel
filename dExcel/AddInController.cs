92191e4ba4522e149db545c493a80705c51edda8
Storm <stcollins@deloitte.co.za>
2022-05-07 20:29:36 +0200

    Added fuzzy logic operators.

---
+++ dExcel/AddInController.cs
@@ -0,0 +1,24 @@
+﻿namespace dExcel;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExcelDna.Integration;
+using Excel = Microsoft.Office.Interop.Excel;
+
+public class AddInController : IExcelAddIn
+{
+    Excel.Application xlapp = (Microsoft.Office.Interop.Excel.Application)ExcelDnaUtil.Application;
+    
+    public void AutoClose()
+    {
+        
+    }
+
+    public void AutoOpen()
+    {
+
+    }
+}
--- dExcel/Curves/Curve.cs
+++ dExcel/Curves/Curve.cs
@@ -10,31 +10,61 @@ using QLNet;
 
 public static class Curve
 {
-    //[ExcelFunction(Name="d.Curve_Create")]
-    //public static string Create(string handle, object[,] datesRange, object[,] discountFactorsRange)
-    //{
-    //    // TODO: Check datesRange and discountFactorsRange have same dimensions.
-    //    List<Date> dates = new();
-    //    List<double> discountFactors = new();
-    //    for (int i = 0; i < datesRange.GetLength(0); i++)
-    //    {
-    //        dates.Add((Date)DateTime.FromOADate((double)datesRange[i, 0]));
-    //        discountFactors.Add((double)discountFactorsRange[i, 0]);
-    //    }
+    [ExcelFunction(
+        Name = "d.Curve_Create",
+        Description = "",
+        Category = "∂Excel: Interest Rates")]
+    public static string Create(string handle, object[,] datesRange, object[,] discountFactorsRange)
+    {
+        // TODO: Check datesRange and discountFactorsRange have same dimensions.
+        List<Date> dates = new();
+        List<double> discountFactors = new();
+        for (int i = 0; i < datesRange.GetLength(0); i++)
+        {
+            dates.Add((Date)DateTime.FromOADate((double)datesRange[i, 0]));
+            discountFactors.Add((double)discountFactorsRange[i, 0]);
+        }
 
-    //    //QLNet.PiecewiseYieldCurve<Discount, LogLinear> curve = new ()
-    //    InterpolatedDiscountCurve<LogLinear> interpolatedDiscountCurve
-    //        = new(dates, discountFactors, new Actual365Fixed(), new LogLinear());
-    //    return DataObjectController.Add(handle, interpolatedDiscountCurve);
-    //}
+        //QLNet.PiecewiseYieldCurve<Discount, LogLinear> curve = new ()
+        InterpolatedDiscountCurve<LogLinear> interpolatedDiscountCurve
+            = new(dates, discountFactors, new Actual365Fixed(), new LogLinear());
+        return DataObjectController.Add(handle, interpolatedDiscountCurve);
+    }
 
-    //[ExcelFunction(Name = "d.Curve_DF")]
-    //public static double GetDiscountFactor(string handle, DateTime date)
-    //{
-    //    return ((InterpolatedDiscountCurve<LogLinear>)DataObjectController.GetDataObject(handle)).discount(date);
-    //}
+    [ExcelFunction(
+        Name = "d.Curve_DF",
+        Description = "",
+        Category = "∂Excel: Interest Rates")]
+    public static object[] GetDiscountFactors(string handle, object[] dates)
+    {
+        var discountFactors = new object[dates.Length];
+        var curve = ((InterpolatedDiscountCurve<LogLinear>)DataObjectController.GetDataObject(handle));
+        for (int i = 0; i < dates.Length; i++)
+        {
+            discountFactors[i] = curve.discount((Date)DateTime.FromOADate((double)dates[i]));
+        }
+        
+        return discountFactors;
+    }
 
-    //[ExcelFunction(Name = "d.Curve_GetForwardRate")]
+    [ExcelFunction(
+        Name = "d.Curve_GetForwardRate",
+        Description = "",
+        Category = "∂Excel: Interest Rates")]
+    public static double GetForwardRate(string handle, DateTime startDate, DateTime endDate)
+    {
+        var curve = ((InterpolatedDiscountCurve<LogLinear>)DataObjectController.GetDataObject(handle));
+        return curve.forwardRate((Date)startDate, (Date)endDate, new Actual365Fixed(), Compounding.Continuous).rate();
+    }
 
+    [ExcelFunction(
+        Name = "d.Curve_GetZeroRate",
+        Description = "",
+        Category = "∂Excel: Interest Rates")]
+    public static double GetZeroRate(string handle, DateTime startDate, DateTime endDate)
+    {
+        var curve = ((InterpolatedDiscountCurve<LogLinear>)DataObjectController.GetDataObject(handle));
+        return curve.zeroRate((Date)startDate, new Actual365Fixed(), Compounding.Continuous).rate();
+    }
 }
 
