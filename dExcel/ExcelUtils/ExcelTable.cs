ba6b6702056e41fe1197f00e9726e8dc910cba71
Storm <stcollins@deloitte.co.za>
2022-06-21 11:42:47 +0200

    Added ExcelTable class and unit tests.

---
+++ dExcel/ExcelUtils/ExcelTable.cs
@@ -0,0 +1,44 @@
+﻿namespace dExcel.ExcelUtils;
+
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+/// A class for manipulating dExcel type tables in Excel.
+/// </summary>
+/// <remarks>Tables are assumed to be of the form:
+/// Table Type
+/// Column Header 1 | Column Header 2 | ... | Column Header n
+/// Value 1         | Value 2         | ... | Value n
+/// ...
+/// </remarks>
+public static class ExcelTable
+{
+    /// <summary>
+    /// Gets the table type.
+    /// </summary>
+    /// <remarks>It is assumed the table type is at position [0,0] in the 2D array.</remarks>
+    /// <param name="table">The input range.</param>
+    /// <returns>The table type.</returns>
+    public static string? GetTableType(object[,] table)
+    {
+        return table[0, 0].ToString();
+    }
+    
+    /// <summary>
+    /// Get the list of column headers of a table.
+    /// </summary>
+    /// <remarks>Assumes the column headers are in row 1 (of a zero based row index).</remarks>
+    /// <param name="table">The input range.</param>
+    /// <returns>The list of column headers.</returns>
+    public static List<string> GetColumnHeaders(object[,] table)
+    {
+        List<string> columnTitles
+                = Enumerable
+                    .Range(0, table.GetLength(1))
+                    .Select(j => table[1, j])
+                    .Cast<string>()
+                    .ToList();
+        return columnTitles;
+    }
+}
---
+++ dExcelTests/Excel/ExcelTableTests.cs
@@ -0,0 +1,26 @@
+﻿namespace dExcelTests.Excel;
+
+using dExcel.ExcelUtils;
+using NUnit.Framework;
+
+[TestFixture]
+public class ExcelTableTests
+{
+    private readonly object[,] _exampleTable =
+    {
+        {"Example Table", ""},
+        {"Parameter", "Value"},
+    };
+    
+    [Test]
+    public void GetTableTypeTest()
+    {
+        Assert.AreEqual("Example Table", ExcelTable.GetTableType(_exampleTable));
+    }
+
+    [Test]
+    public void GetColumnTitlesTest()
+    {
+        Assert.AreEqual(new List<string> {"Parameter", "Value"}, ExcelTable.GetColumnHeaders(_exampleTable));
+    }
+}
