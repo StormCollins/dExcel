9e4e15c535057cac77a4506a7e9749271f5c162a
Storm <stcollins@deloitte.co.za>
2022-06-21 18:00:11 +0200

    Updated Excel template with 'Include' columns in instrument tables.

--- dExcelTests/Curves/SingleCurveBootstrapperTests.cs
+++ dExcelTests/Curves/SingleCurveBootstrapperTests.cs
@@ -95,4 +95,63 @@ public class SingleCurveBootstrapperTests
             actual: curve.discount(baseDate.AddMonths(9)),
             delta: tolerance);
     }
+    
+    [Test]
+    public void BootstrapFlatCurveDepositsFrasAndSwapsTest()
+    {
+        var baseDate = new DateTime(2022, 06, 01);
+        object[,] depositInstruments = 
+        {
+            {"Deposits", "", "", ""},
+            {"Tenors", "RateIndex", "Rates", "Include"},
+            {"1m", "JIBAR", 0.1, "TRUE"},
+            {"3m", "JIBAR", 0.1, "TRUE"},
+            {"6m", "JIBAR", 0.1, "TRUE"},
+        };
+       
+        object[,] fraInstruments = 
+        {
+            {"FRAs", "", "", ""},
+            {"Tenors", "RateIndex", "Rates", "Include"},
+            {"6m", "JIBAR", 0.1, "TRUE"},
+            {"9m", "JIBAR", 0.1, "TRUE"},
+        };
+
+        object[,] swapInstruments =
+        {
+            {"Interest Rate Swaps", "", "", ""},
+            {"Tenors", "RateIndex", "Rates", "Include"},
+            {"1y", "JIBAR", 0.1, "TRUE"},
+            {"2y", "JIBAR", 0.1, "TRUE"},
+        };
+
+        object[] instruments = {depositInstruments, fraInstruments, swapInstruments};
+        var dayCounter = new Actual365Fixed();
+        var handle = SingleCurveBootstrapper.Bootstrap("BootstrappedSingleCurve", baseDate, instruments);
+        var curve = (YieldTermStructure)((Dictionary<string, object>)DataObjectController.GetDataObject(handle))["Curve"];
+        const double tolerance = 0.01; 
+        
+        Assert.AreEqual(1.0, curve.discount(baseDate));
+        Assert.AreEqual(
+            expected: Math.Exp(-0.1 * dayCounter.yearFraction(baseDate, baseDate.AddMonths(1))),
+            actual: curve.discount(baseDate.AddMonths(1)),
+            delta: tolerance);
+        Assert.AreEqual(
+            expected: Math.Exp(-0.1 * dayCounter.yearFraction(baseDate, baseDate.AddMonths(2))),
+            actual: curve.discount(baseDate.AddMonths(2)),
+            delta: tolerance);
+        Assert.AreEqual(
+            expected: Math.Exp(-0.1 * dayCounter.yearFraction(baseDate, baseDate.AddMonths(3))),
+            actual: curve.discount(baseDate.AddMonths(3)),
+            delta: tolerance);
+        Assert.AreEqual(
+            expected: Math.Exp(-0.1 * dayCounter.yearFraction(baseDate, baseDate.AddMonths(6))),
+            actual: curve.discount(baseDate.AddMonths(6)),
+            delta: tolerance);
+        Assert.AreEqual(
+            expected: Math.Exp(-0.1 * dayCounter.yearFraction(baseDate, baseDate.AddMonths(6))) *
+                      Math.Exp(-0.1 * dayCounter.yearFraction(baseDate.AddMonths(6), baseDate.AddMonths(9))),
+            actual: curve.discount(baseDate.AddMonths(9)),
+            delta: tolerance);
+    }
 }
