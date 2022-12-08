5b388b803bef5ee55a0a38274cbac1e810c12df7
Storm Collins <stcollins@deloitte.co.za>
2022-12-08 19:43:51 +0200

    Updated packages.

--- dExcel/Dates/Calendars/Nigeria.cs
+++ dExcel/Dates/Calendars/Nigeria.cs
@@ -2,8 +2,6 @@
 
 using System.Globalization;
 using ExcelDna.Integration;
-using NodaTime;
-using NodaTime.Calendars;
 
 public static class Nigeria
 {
@@ -12,7 +10,6 @@ public static class Nigeria
    public static DateTime Test()
    {
       HijriCalendar calendars = new HijriCalendar();
-      var x = CalendarSystem.GetIslamicCalendar(IslamicLeapYearPattern.Base15, IslamicEpoch.Astronomical);
       DateTime d = new(2022, 12, 05);
       var hijriMonth = calendars.GetMonth(d);
       var hijriYear = calendars.GetDayOfMonth(d);
