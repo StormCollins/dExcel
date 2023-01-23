49a84b5324c75f612e82440ac88b15ced7b0d4ea
Storm Collins <stcollins@deloitte.co.za>
2023-01-23 11:35:01 +0200

    Added reportgenerator to CI YAML file.

--- Tools/get_coverage_stats.py
+++ Tools/get_coverage_stats.py
@@ -1,11 +1,12 @@
 import re
 
-f = open(r'CoverageResults\results.xml', 'r')
+f = open(r'publish\CoverageResults\results.xml', 'r')
 lines = f.readlines()
 
 if not re.match('.+Summary.+', lines[2]):
     print(f'Line 2 of results.xml no longer contains the coverage session summary. ' +
           f'Update {__file__}.')
 else:
+    print(lines[2])
     sequence_coverage = re.findall(r'(?<=sequenceCoverage=")\d+\.\d+', lines[2])[0]
-    print(sequence_coverage)
+    print(f'Sequence Coverage: {sequence_coverage}')
