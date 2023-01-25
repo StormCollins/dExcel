5dd2eaaae037610dcf62d87b3542ac353d390fbb
Storm Collins <stcollins@deloitte.co.za>
2023-01-25 10:33:11 +0000

    Update .gitlab-ci.yml file

--- Tools/get_coverage_stats.py
+++ Tools/get_coverage_stats.py
@@ -3,15 +3,10 @@ from lxml import etree
 import os
 
 print(f'Current Working Directory: {os.getcwd()}')
-print('')
-print(f'Files/folders in current directory:')
-for file in os.scandir('publish/CoverageResults/Report'):
-    print(file)
-
-print('')
 report_path: str = r'publish\CoverageResults\Report\index.html'
 f = open(report_path, 'r')
 page = ''.join(f.readlines())
 soup = BeautifulSoup(page, features='lxml')
 dom = etree.HTML(str(soup))
-print(f'Coverage: {dom.xpath(r"//html/body/div[1]/div/div[1]/div[2]/div[2]/div[1]/text()")[0]}')
+coverage = dom.xpath(r"//html/body/div[1]/div/div[1]/div[2]/div[2]/div[2]/table/tr[5]/td/text()")[0]
+print(f'Coverage: {coverage}')
