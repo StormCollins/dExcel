4f00efab47c84e578e554ce2c40d7f77500a9690
Storm Collins <stcollins@deloitte.co.za>
2023-02-14 15:12:53 +0200

    Added test for TryParseOptionTypeToSign

--- dExcelTests/Utilities/CommonUtilsTests.cs
+++ dExcelTests/Utilities/CommonUtilsTests.cs
@@ -7,6 +7,22 @@ using QLNet;
 [TestFixture]
 public class CommonUtilsTests
 {
+    [Test]
+    [TestCase("Call", true, 1, null)]
+    [TestCase("CALL", true, 1, null)]
+    [TestCase("Put", true, -1, null)]
+    [TestCase("PUT", true, -1, null)]
+    [TestCase("Invalid", false, null, "#∂Excel Error: Invalid option type: 'Invalid'")]
+    public void TryParseOptionTypeToSign_Test(string optionType, bool expectedResult, int? expectedSign, string? expectedErrorMessage)
+    {
+        bool actualResult = 
+            CommonUtils.TryParseOptionTypeToSign(optionType, out int? actualSign, out string? actualErrorMessage);
+        Assert.AreEqual(expectedResult, actualResult);
+        Assert.AreEqual(expectedSign, actualSign);
+        Assert.AreEqual(expectedErrorMessage, actualErrorMessage);
+    }
+    
+    
     // Since we have to test the "out" parameters as well, there doesn't seem to be an elegant, easily readable way to
     // do this, other than just testing each case in turn.
     [Test]
@@ -14,7 +30,7 @@ public class CommonUtilsTests
     [TestCase("ACT360")]
     [TestCase("Actual360")]
     [TestCase("ACTUAL360")]
-    public void TryParseDayCountConventionForActual360Test(string dayCountConventionToParse)
+    public void TryParseDayCountConvention_Actual360Test(string dayCountConventionToParse)
     {
         bool actual = CommonUtils.TryParseDayCountConvention(dayCountConventionToParse, out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsTrue(actual);
@@ -29,7 +45,7 @@ public class CommonUtilsTests
     [TestCase("ACT365")]
     [TestCase("Actual365")]
     [TestCase("ACTUAL365")]
-    public void TryParseDayCountConventionForActual365Test(string dayCountConventionToParse)
+    public void TryParseDayCountConvention_Actual365Test(string dayCountConventionToParse)
     {
         bool actual = CommonUtils.TryParseDayCountConvention(dayCountConventionToParse, out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsTrue(actual);
@@ -44,7 +60,7 @@ public class CommonUtilsTests
     [TestCase("ActAct")]
     [TestCase("ActualActual")]
     [TestCase("ACTUALACTUAL")]
-    public void TryParseDayCountConventionForActualActualTest(string dayCountConventionToParse)
+    public void TryParseDayCountConvention_ActualActualTest(string dayCountConventionToParse)
     {
         bool actual = CommonUtils.TryParseDayCountConvention(dayCountConventionToParse, out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsTrue(actual);
@@ -57,7 +73,7 @@ public class CommonUtilsTests
     [Test]
     [TestCase("Business252")]
     [TestCase("BUSINESS252")]
-    public void TryParseDayCountConventionForBusiness252Test(string dayCountConventionToParse)
+    public void TryParseDayCountConvention_Business252Test(string dayCountConventionToParse)
     {
         bool actual = CommonUtils.TryParseDayCountConvention(dayCountConventionToParse, out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsTrue(actual);
@@ -71,7 +87,7 @@ public class CommonUtilsTests
     [TestCase("30360")]
     [TestCase("Thirty360")]
     [TestCase("THIRTY360")]
-    public void TryParseDayCountConvention30360Test(string dayCountConventionToParse)
+    public void TryParseDayCountConvention_30360Test(string dayCountConventionToParse)
     {
         bool actual = CommonUtils.TryParseDayCountConvention(dayCountConventionToParse, out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsTrue(actual);
@@ -82,7 +98,7 @@ public class CommonUtilsTests
     // Since we have to test the "out" parameters as well, there doesn't seem to be an elegant, easily readable way to
     // do this, other than just testing each case in turn.
     [Test]
-    public void TryParseDayCountConventionErrorMessageTest()
+    public void TryParseDayCountConvention_ErrorMessageTest()
     {
         bool actual = CommonUtils.TryParseDayCountConvention("Invalid", out DayCounter? actualDayCountConvention, out string? errorMessage); 
         Assert.IsFalse(actual);
